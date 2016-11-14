46f954b1247182cac3306af03af650d26b3df502
kszymanski <kszymanski@future-processing.com>
2016-11-14 14:46:26 +0100

    'initial'

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import '../../public/css/styles.css';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    constructor(){
+      console.log('hello');
+      let dupa = '123';
+    }
+
+ }
